438d23218bdfad1a8dacee568fd70491bb09df65
Michał Górny <mgorny@gentoo.org>
2024-07-15 20:37:38 +0200

    test: Move common stuff to conftest
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,32 +1,7 @@
 # (c) 2024 Michał Górny
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-import functools
-
 import pytest
-
-
-pytest_plugins = ["pytester"]
-
-
-@pytest.fixture(params=["importlib", "prepend"])
-def import_mode(request):
-    yield request.param
-
-
-@pytest.fixture(params=["false", "true"])
-def consider_namespace_packages(request):
-    yield request.param
-
-
-@pytest.fixture
-def run(pytester, import_mode, consider_namespace_packages):
-    pytester.syspathinsert()
-    yield functools.partial(pytester.runpytest,
-                            "-vv", "--tb=long", "--import-check",
-                            f"--import-mode={import_mode}",
-                            "--override-ini=consider_namespace_packages="
-                            f"{consider_namespace_packages}")
 
 
 def test_no_imports(run, pytester):
