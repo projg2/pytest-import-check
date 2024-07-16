49d8ba7bb8c50b94da42fc9c9a48212f7beda81e
Michał Górny <mgorny@gentoo.org>
2024-07-16 09:36:34 +0200

    test: Convert build_c_ext into a fixture
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- test/test_c_ext.py
+++ test/test_c_ext.py
@@ -4,41 +4,46 @@
 import subprocess
 import sys
 
+import pytest
 
+
+@pytest.fixture
 def build_c_ext(pytester):
-    pytester.makefile(".c", test="""
-        #define PY_SSIZE_T_CLEAN
-        #include <Python.h>
+    def inner():
+        pytester.makefile(".c", test="""
+            #define PY_SSIZE_T_CLEAN
+            #include <Python.h>
 
-        static struct PyModuleDef testmodule = {
-            PyModuleDef_HEAD_INIT,
-            .m_name = "test",
-            .m_doc = NULL,
-            .m_size = -1,
-            .m_methods = NULL,
-        };
+            static struct PyModuleDef testmodule = {
+                PyModuleDef_HEAD_INIT,
+                .m_name = "test",
+                .m_doc = NULL,
+                .m_size = -1,
+                .m_methods = NULL,
+            };
 
-        PyMODINIT_FUNC
-        PyInit_test(void)
-        {
-            return PyModule_Create(&testmodule);
-        }
-    """)
-    pytester.makepyfile(setup="""
-        from setuptools import setup, Extension
+            PyMODINIT_FUNC
+            PyInit_test(void)
+            {
+                return PyModule_Create(&testmodule);
+            }
+        """)
+        pytester.makepyfile(setup="""
+            from setuptools import setup, Extension
 
-        setup(name="test",
-              version="0",
-              ext_modules=[
-                  Extension(name="test",
-                            sources=["test.c"]),
-              ])
-    """)
-    subprocess.run([sys.executable, "setup.py", "build_ext", "-i"],
-                   check=True)
+            setup(name="test",
+                  version="0",
+                  ext_modules=[
+                      Extension(name="test",
+                                sources=["test.c"]),
+                  ])
+        """)
+        subprocess.run([sys.executable, "setup.py", "build_ext", "-i"],
+                       check=True)
+    yield inner
 
 
-def test_c_ext(run, pytester):
-    build_c_ext(pytester)
+def test_c_ext(run, build_c_ext):
+    build_c_ext()
     result = run("--ignore=setup.py")
     result.assert_outcomes(passed=1)
